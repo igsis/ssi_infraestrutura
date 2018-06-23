ebc4d98615a353ca8194e946ed836b7ff77f4fb6
Lorelei Gabriele <loreleigab@hotmail.com>
2018-06-23 10:24:57 -0300

    Menu provisório do usuário

---
+++ perfil/usuario/includes/menu.php
@@ -0,0 +1,19 @@
+<?php
+//geram o insert pro framework da igsis
+$pasta = "?perfil=usuario&p=";
+ ?>
+
+<div class="menu-area">
+	<div id="dl-menu" class="dl-menuwrapper">
+		<button class="dl-trigger">Open Menu</button>
+		<ul class="dl-menu">
+			<li><a href="?perfil=usuario">Home</a></li>
+			<li><a href="<?php echo $pasta ?>cadastra_chamado">Enviar chamado</a></li>
+			<li><a href="#">Lista de Chamados</a></li>
+			<li style="color:white;">-------------------------</li>
+			<li><a href="index.php?secao=perfil">Minhas informações</a></li>
+			<li><a href="#">Manual do sistema</a></li>
+			<li><a href="../index.php">Sair</a></li>
+		</ul>
+	</div>
+</div>
