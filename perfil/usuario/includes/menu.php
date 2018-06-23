58c0d4b4d69a264edc93a22a37a93722ad939ba5
Lorelei Gabriele <loreleigab@hotmail.com>
2018-06-23 19:11:59 -0300

    Ajuste no menu do usuário

--- perfil/usuario/includes/menu.php
+++ perfil/usuario/includes/menu.php
@@ -10,8 +10,9 @@ $pasta = "?perfil=usuario&p=";
 			<li><a href="?perfil=usuario">Home</a></li>
 			<li><a href="<?php echo $pasta ?>cadastra_chamado">Enviar chamado</a></li>
 			<li><a href="<?php echo $pasta ?>lista_chamado">Lista de Chamados</a></li>
+			<li><a href="<?php echo $pasta ?>busca">Busca</a></li>
 			<li style="color:white;">-------------------------</li>
-			<li><a href="index.php?secao=perfil">Minhas informações</a></li>
+			<li><a href="<?php echo $pasta ?>minhas_informacoes">">Minhas informações</a></li>
 			<li><a href="#">Manual do sistema</a></li>
 			<li><a href="../index.php">Sair</a></li>
 		</ul>
