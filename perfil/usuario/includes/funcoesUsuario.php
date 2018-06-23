b48a278bf031dc921535c1a20bf8163261336acc
Lorelei Gabriele <loreleigab@hotmail.com>
2018-06-23 12:22:34 -0300

    Função do Usuário para recuperar as informações do cadastro

---
+++ perfil/usuario/includes/funcoesUsuario.php
@@ -0,0 +1,30 @@
+<?php
+function recuperaInfoUsuario($idUser)
+{
+	//retorna uma array com indice 'numero' de registros e 'dados' da tabela
+	$con = bancoMysqli();
+	$sql = "SELECT * FROM users WHERE id = '$idUser'";
+	$query = mysqli_query($con,$sql);
+	$lista = mysqli_fetch_array($query);
+
+	$sql_admin_user = "SELECT `users_id`, `admininstrators_id` FROM `administrators_users` WHERE `users_id` = $idUser";
+	$query_admin_user = mysqli_query($con,$sql_admin_user);
+	$adminUser = mysqli_fetch_array($query_admin_user);
+
+	$campo = array(
+		"idUser" => $lista['id'],
+		"login" => $lista['login'],
+		"local" => $lista['local'],
+		"phone" => $lista['phone'],
+		"email" => $lista['email'],
+		"contact" => $lista['contact'],
+		"idRegion" => $lista['idRegion'],
+		"operantingHours" => $lista['operantingHours'],
+		"historicalBuilding" => $lista['historicalBuilding'],
+		"dateCreated" => $lista['dateCreated'],
+		"dateLastAccess" => $lista['dateLastAccess'],
+		"idAdminUser" => $adminUser['admininstrators_id']
+	);
+	return $campo;
+}
+?>
