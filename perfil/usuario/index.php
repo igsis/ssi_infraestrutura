c1388bab3ddc5f47786c25e9de9d2407cb762cf3
Lorelei Gabriele <loreleigab@hotmail.com>
2018-06-23 10:25:30 -0300

    Index provisória do usuário

--- perfil/usuario/index.php
+++ perfil/usuario/index.php
@@ -1,4 +1,4 @@
-<?php //include 'includes/menu.php';?>
+<?php include 'includes/menu.php';?>
 
 <section id="list_items" class="home-section bg-white">
 	<div class="container">
@@ -8,8 +8,15 @@
 					<h3>Bem-vindo(a) ao sistema!</h3>
                     <p>&nbsp;</p>
                     <h2>Módulo Usuário</h2>
-                    <p>&nbsp;</p>
-					<p>Esse módulo tem a função de gerenciar a produção de contratos artísticos da instituição, como propostas, declarações, pedidos de reserva de empenho entre outros documentos necessários para a contratação do artista.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<a href="?perfil=usuario&p=cadastra_chamado">Enviar chamado</a>
+					</div>
 				</div>
 			</div>
 		</div>
