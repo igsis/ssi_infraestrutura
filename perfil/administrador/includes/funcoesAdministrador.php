436929bca48f9f15a9f32b996ffc482bd7c7578b
Lorelei Gabriele <loreleigab@hotmail.com>
2018-06-24 12:41:56 -0300

    Função Adm para listar os usuários dele

--- perfil/administrador/includes/funcoesAdministrador.php
+++ perfil/administrador/includes/funcoesAdministrador.php
@@ -1,3 +1,21 @@
 <?php
+function listaUser($idAdm,$select)
+{
+	//gera os options de um select
+	$sql = "SELECT * FROM users INNER JOIN administrators_users ON users.id = administrators_users.users_id WHERE administrators_users.admininstrators_id = $idAdm";
 
+	$con = bancoMysqli();
+	$query = mysqli_query($con,$sql);
+	while($option = mysqli_fetch_row($query))
+	{
+		if($option[0] == $select)
+		{
+			echo "<option value='".$option[0]."' selected >".$option[1]."</option>";
+		}
+		else
+		{
+			echo "<option value='".$option[0]."'>".$option[1]."</option>";
+		}
+	}
+}
 ?>
