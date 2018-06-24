01e268b2640619055a5d44a86b21b1c1de2809df
Lorelei Gabriele <loreleigab@hotmail.com>
2018-06-24 11:51:28 -0300

    Arquivos iniciais do administrador

---
+++ perfil/administrador/includes/menu.php
@@ -0,0 +1,26 @@
+<?php
+//geram o insert pro framework da igsis
+$pasta = "?perfil=administrador&p=";
+ ?>
+
+<div class="menu-area">
+	<div id="dl-menu" class="dl-menuwrapper">
+		<button class="dl-trigger">Open Menu</button>
+		<ul class="dl-menu">
+			<li><a href="?perfil=usuario">Home</a></li>
+			<li><a href="<?php echo $pasta ?>cadastra">Enviar chamado</a></li>
+			<li><a href="<?php echo $pasta ?>lista">Lista de Chamados</a></li>
+			<li><a href="<?php echo $pasta ?>busca">Busca</a></li>
+			<li><a href="#">Acesso Administrativo</a>
+				<ul class="dl-submenu">
+					<li><a href="<?php echo $pasta ?>busca">Categoria</a></li>
+					<li><a href="<?php echo $pasta ?>busca">Local</a></li>
+				</ul>
+			</li>
+			<li style="color:white;">-------------------------</li>
+			<li><a href="<?php echo $pasta ?>minhas_informacoes">Minhas informações</a></li>
+			<li><a href="#">Manual do sistema</a></li>
+			<li><a href="../index.php">Sair</a></li>
+		</ul>
+	</div>
+</div>
---
+++ perfil/administrador/index.php
@@ -0,0 +1,30 @@
+<?php
+?>
+<section id="list_items" class="home-section bg-white">
+	<div class="container"><?php include 'includes/menu.php';?>
+	<p align="left"><strong><?php echo saudacao(); ?>, <?php echo $_SESSION['nome']; ?>!</strong></p>
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8">
+				<div class="text-hide">
+                    <h2>Módulo Administrador</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div align="left" class="col-md-offset-1 col-md-10">
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8"><h4>Estatísticas</h4>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-3">Chamados Abertos
+					</div>
+					<div class="col-md-3">Chamados Em Andamento
+					</div>
+					<div class="col-md-3">Chamados Fechados
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
