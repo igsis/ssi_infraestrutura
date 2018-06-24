8aae3b766084ff70d1ad375fe5a92a400316b74d
Lorelei Gabriele <loreleigab@hotmail.com>
2018-06-23 21:17:29 -0300

    Continuação do index do usuário

--- perfil/usuario/index.php
+++ perfil/usuario/index.php
@@ -1,21 +1,27 @@
-<?php include 'includes/menu.php';?>
-
+<?php
+?>
 <section id="list_items" class="home-section bg-white">
-	<div class="container">
+	<div class="container"><?php include 'includes/menu.php';?>
+	<p align="left"><strong><?php echo saudacao(); ?>, <?php echo $_SESSION['nome']; ?>!</strong></p>
 		<div class="row">
 			<div class="col-md-offset-2 col-md-8">
 				<div class="text-hide">
-					<h3>Bem-vindo(a) ao sistema!</h3>
-                    <p>&nbsp;</p>
                     <h2>Módulo Usuário</h2>
 				</div>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-offset-1 col-md-10">
+			<div align="left" class="col-md-offset-1 col-md-10">
 				<div class="form-group">
-					<div class="col-md-offset-2 col-md-8">
-						<a href="?perfil=usuario&p=cadastra_chamado">Enviar chamado</a>
+					<div class="col-md-offset-2 col-md-8"><h4>Estatísticas</h4>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-3">Chamados Abertos
+					</div>
+					<div class="col-md-3">Chamados Em Andamento
+					</div>
+					<div class="col-md-3">Chamados Fechados
 					</div>
 				</div>
 			</div>
