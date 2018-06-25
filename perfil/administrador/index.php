c9d83737695cb18144b91597209940f5d0092f74
Lucas Jesus <lucas_ap@live.com>
2018-06-25 13:50:37 -0300

    index módulo adm atualizado

--- perfil/administrador/index.php
+++ perfil/administrador/index.php
@@ -1,30 +1,82 @@
 <?php
 ?>
 <section id="list_items" class="home-section bg-white">
-	<div class="container"><?php include 'includes/menu.php';?>
-	<p align="left"><strong><?php echo saudacao(); ?>, <?php echo $_SESSION['nome']; ?>!</strong></p>
-		<div class="row">
-			<div class="col-md-offset-2 col-md-8">
-				<div class="text-hide">
+    <div class="container"><?php include 'includes/menu.php';?>
+        <p align="left"><strong><?php echo saudacao(); ?>, <?php echo $_SESSION['nome']; ?>!</strong></p>
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8">
+                <div class="text-hide">
                     <h2>Módulo Administrador</h2>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div align="left" class="col-md-offset-1 col-md-10">
-				<div class="form-group">
-					<div class="col-md-offset-2 col-md-8"><h4>Estatísticas</h4>
-					</div>
-				</div>
-				<div class="form-group">
-					<div class="col-md-offset-2 col-md-3">Chamados Abertos
-					</div>
-					<div class="col-md-3">Chamados Em Andamento
-					</div>
-					<div class="col-md-3">Chamados Fechados
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                </div>
+            </div>
+        </div>
+
+        <?php
+        $id1 = $_SESSION['idAdm'];
+        ?>
+
+        <?php
+        $aberto = "SELECT id FROM problems WHERE administrators_id='$id1' AND problem_status_id ='1'"; // Registro de chamados em abertos
+        $con = bancoMysqli();
+        $query1 = mysqli_query($con,$aberto);
+
+        $qtde1 = mysqli_num_rows($query1);
+
+        ?>
+
+        <?php
+        $fechado = "SELECT id FROM problems WHERE administrators_id='$id1' AND problem_status_id ='2'"; // Registro de chamados fechado
+        $con = bancoMysqli();
+        $query2 = mysqli_query($con,$fechado);
+
+        $qtde2 = mysqli_num_rows($query2);
+
+        ?>
+
+        <?php
+        $andamento = "SELECT id FROM problems WHERE administrators_id='$id1' AND problem_status_id ='3'"; // Registro de chamado em andamento
+        $con = bancoMysqli();
+        $query3 = mysqli_query($con,$andamento);
+
+        $qtde3 = mysqli_num_rows($query3);
+
+        ?>
+
+
+
+        <div class="col-md-offset-2 col-md-8">
+            <div class="card border border-secondary">
+                <table class="table table-borderless">
+                    <thead class="bg-dark">
+                    <tr>
+                        <th scope="col" class="text-center" style="color: white">Estatisticas</th>
+                        <th scope="col" class="text-center" style="color: white">Total</th>
+
+
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row" class="text-center">Abertos</th>
+                        <td class="text-center"><?php echo $qtde1; ?></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-center">Em Andamento</th>
+                        <td class="text-center"><?php echo $qtde2; ?></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row" class="text-center">Fechado</th>
+                        <td class="text-center"><?php echo $qtde3; ?></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <br><br><br><br>
+
+    </div>
+    </div>
+    </div>
 </section>
