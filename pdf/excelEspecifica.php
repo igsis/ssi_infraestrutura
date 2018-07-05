7d7064ba46146a1ff567f2476a033442593b01da
Lucas Jesus <lucas_ap@live.com>
2018-07-05 00:59:27 -0300

    ajustado o sistema de relatório

--- pdf/excelEspecifica.php
+++ pdf/excelEspecifica.php
@@ -1,146 +1,133 @@
 <?php
 //require '../include/';
 require_once("../funcoes/funcoesConecta.php");
-require_once("../funcoes/funcoesGerais.php");
 require_once("../include/phpexcel/Classes/PHPExcel.php");
-
 
 //CONEXÃO COM BANCO DE DADOS
 $con = bancoMysqli();
 
-if(isset($_POST['relatorio'])){
-
 // Create new PHPExcel object
 $objPHPExcel = new PHPExcel();
-// Set document properties
-$cacheMethod = PHPExcel_CachedObjectStorageFactory:: cache_to_phpTemp;
-$cacheSettings = array( ' memoryCacheSize ' => '8MB');
-PHPExcel_Settings::setCacheStorageMethod($cacheMethod, $cacheSettings);
-$objPHPExcel->getProperties()->setCreator("Sistema SSI - Infraestrutura");
-$objPHPExcel->getProperties()->setLastModifiedBy("Sistema SSI - Infraestrutura");
-$objPHPExcel->getProperties()->setTitle("Relatório de Chamados");
-$objPHPExcel->getProperties()->setSubject("Relatório de Chamados");
-$objPHPExcel->getProperties()->setDescription("Gerado automaticamente a partir do Sistema SSI - Infraestrutura");
-$objPHPExcel->getProperties()->setKeywords("office 2007 openxml php");
-$objPHPExcel->getProperties()->setCategory("Relatório de Chamados");
 
-// Add some data
-$objPHPExcel->setActiveSheetIndex(0)
-    ->setCellValue('A1', 'Chamado nº')
-    ->setCellValue('B1', 'Usuário')
-    ->setCellValue('C1', 'Local')
-    ->setCellValue('D1', 'Telefone')
-    ->setCellValue('E1', 'Email')
-    ->setCellValue('F1', 'Contato')
-    ->setCellValue('G1', 'Propriedade')
-    ->setCellValue('H1', 'Categoria')
-    ->setCellValue('I1', 'Descrição')
-    ->setCellValue('J1', 'Solução')
-    ->setCellValue('K1', 'Status')
-    ->setCellValue('L1', 'Data de Envio');
-
-//Colorir a primeira fila
-$objPHPExcel->getActiveSheet()->getStyle('A1:L1')->applyFromArray(
-    array(
-        'fill' => array(
-            'type' => PHPExcel_Style_Fill::FILL_SOLID,
-            'color' => array('rgb' => 'E0EEEE')
-        ),
-    )
-);
-
-    foreach($_POST['relatorio'] as $id => $relatorio){
-//Dados
-$sql = "SELECT * FROM problems WHERE id = '$relatorio' LIMIT 1";
-$query = mysqli_query($con,$sql);
-$i = 2; // para começar a gravar os dados na segunda linha
-while($row = mysqli_fetch_array($query)) {
-
-
-    $sql2= "SELECT * FROM categories WHERE id = " . $row["categories_id"];
-    $res2 = mysqli_query($con, $sql2);
-    $reg2 = mysqli_fetch_assoc($res2);
-
-    $sql3= "SELECT * FROM priorities WHERE id = " . $row["priorities_id"];
-    $res3 = mysqli_query($con, $sql3);
-    $reg3 = mysqli_fetch_assoc($res3);
-
-    $sql4= "SELECT * FROM problem_status WHERE id = " . $row["problem_status_id"];
-    $res4 = mysqli_query($con, $sql4);
-    $reg4 = mysqli_fetch_assoc($res4);
-
-    $sql5= "SELECT * FROM users WHERE id = " . $row["users_id"];
-    $res5 = mysqli_query($con, $sql5);
-    $reg5 = mysqli_fetch_assoc($res5);
-
-
-    $id_chamado = $row["id"];
-    $id_users = $row['users_id'];
-    $local = $row['local'];
-    $telefone = $row['phone'];
-    $email = $row['email'];
-    $contato = $row['contact'];
-    $prioriedade = $row['priorities_id'];
-    $categoria = $row['categories_id'];
-    $descricao = $row["description"];
-    $solucao = $row["solution"];
-    $status = $row["problem_status_id"];
-    $data = date('d/m/Y H:i:s', strtotime($row["startDate"]));
-
-    $a = "A".$i;
-    $b = "B".$i;
-    $c = "C".$i;
-    $d = "D".$i;
-    $e = "E".$i;
-    $f = "F".$i;
-    $g = "G".$i;
-    $h = "H".$i;
-    $I = "I".$i;
-    $j = "J".$i;
-    $k = "K".$i;
-    $l = "L".$i;
-
+if(isset($_POST['relatorio'])){
+    // Definimos o nome do arquivo que será exportado
+    $arquivo = 'Relatorio.xls';
+    // Criamos uma tabela HTML com o formato da planilha
     $objPHPExcel->setActiveSheetIndex(0)
-        ->setCellValue($a, $row['id'])
-        ->setCellValue($b, $reg5['login'])
-        ->setCellValue($c, $row['local'])
-        ->setCellValue($d, $row['phone'])
-        ->setCellValue($e, $row['email'])
-        ->setCellValue($f, $row['contact'])
-        ->setCellValue($g, $reg3['priority'])
-        ->setCellValue($h, $reg2['category'])
-        ->setCellValue($I, $row['description'])
-        ->setCellValue($j, $row['solution'])
-        ->setCellValue($k, $reg4['status'])
-        ->setCellValue($l, $data);
-    $i++;
-}}
-foreach (range('A', $objPHPExcel->getActiveSheet()->getHighestDataColumn()) as $col)
-{
-    $objPHPExcel->getActiveSheet()
-        ->getColumnDimension($col)
-        ->setAutoSize(true);
-}
-$objPHPExcel->setActiveSheetIndex(0);
-ob_end_clean();
-ob_start();
-$nome_arquivo = "Relatorio.xls";
-// Redirect output to a client’s web browser (Excel2007)
-header('Content-Type: text/html; charset=ISO-8859-1');
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="'.$nome_arquivo.'"');
-header('Cache-Control: max-age=0');
-// If you're serving to IE 9, then the following may be needed
-header('Cache-Control: max-age=1');
-// If you're serving to IE over SSL, then the following may be needed
-header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
-header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-header ('Pragma: public'); // HTTP/1.0
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-$objWriter->save('php://output');
-}else{
-    echo"<script> alert('Nenhum Item Selecionado!');</script>";
+        ->setCellValue('A1', 'Chamado nº')
+        ->setCellValue('B1', 'Usuário')
+        ->setCellValue('C1', 'Local')
+        ->setCellValue('D1', 'Telefone')
+        ->setCellValue('E1', 'Email')
+        ->setCellValue('F1', 'Contato')
+        ->setCellValue('G1', 'Propriedade')
+        ->setCellValue('H1', 'Categoria')
+        ->setCellValue('I1', 'Descrição')
+        ->setCellValue('J1', 'Solução')
+        ->setCellValue('K1', 'Status')
+        ->setCellValue('L1', 'Data de Envio');
+//Colorir a primeira fila
+    $objPHPExcel->getActiveSheet()->getStyle('A1:L1')->applyFromArray(
+        array(
+            'fill' => array(
+                'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                'color' => array('rgb' => 'E0EEEE')
+            ),
+        )
+    );$i = 2;
+    foreach($_POST['relatorio'] as $id => $msg_contato){
+        //echo "ID do item: $id <br>";
+        //Selecionar todos os itens da tabela
+        $result = "SELECT * FROM problems WHERE id = $id LIMIT 1";
+        $resultado = mysqli_query($con , $result);
 
+        while($row = mysqli_fetch_assoc($resultado)){
+            $sql= "SELECT * FROM administrators WHERE id = " . $row["administrators_id"];
+            $res = mysqli_query($con, $sql);
+            $reg = mysqli_fetch_assoc($res);
+            $sql2= "SELECT * FROM categories WHERE id = " . $row["categories_id"];
+            $res2 = mysqli_query($con, $sql2);
+            $reg2 = mysqli_fetch_assoc($res2);
+            $sql3= "SELECT * FROM priorities WHERE id = " . $row["priorities_id"];
+            $res3 = mysqli_query($con, $sql3);
+            $reg3 = mysqli_fetch_assoc($res3);
+            $sql4= "SELECT * FROM problem_status WHERE id = " . $row["problem_status_id"];
+            $res4 = mysqli_query($con, $sql4);
+            $reg4 = mysqli_fetch_assoc($res4);
+            $sql5= "SELECT * FROM users WHERE id = " . $row["users_id"];
+            $res5 = mysqli_query($con, $sql5);
+            $reg5 = mysqli_fetch_assoc($res5);
+
+            $id_chamado = $row["id"];
+            $id_users = $row['users_id'];
+            $local = $row['local'];
+            $telefone = $row['phone'];
+            $email = $row['email'];
+            $contato = $row['contact'];
+            $prioriedade = $row['priorities_id'];
+            $categoria = $row['categories_id'];
+            $descricao = $row["description"];
+            $solucao = $row["solution"];
+            $status = $row["problem_status_id"];
+            $data = date('d/m/Y H:i:s', strtotime($row["startDate"]));
+
+
+            $a = "A".$i;
+            $b = "B".$i;
+            $c = "C".$i;
+            $d = "D".$i;
+            $e = "E".$i;
+            $f = "F".$i;
+            $g = "G".$i;
+            $h = "H".$i;
+            $I = "I".$i;
+            $j = "J".$i;
+            $k = "K".$i;
+            $l = "L".$i;
+
+
+            $objPHPExcel->setActiveSheetIndex(0)
+                ->setCellValue($a, $row['id'])
+                ->setCellValue($b, $reg5['login'])
+                ->setCellValue($c, $row['local'])
+                ->setCellValue($d, $row['phone'])
+                ->setCellValue($e, $row['email'])
+                ->setCellValue($f, $row['contact'])
+                ->setCellValue($g, $reg3['priority'])
+                ->setCellValue($h, $reg2['category'])
+                ->setCellValue($I, $row['description'])
+                ->setCellValue($j, $row['solution'])
+                ->setCellValue($k, $reg4['status'])
+                ->setCellValue($l, $data);
+            $i++;
+
+    foreach (range('A', $objPHPExcel->getActiveSheet()->getHighestDataColumn()) as $col)
+    {
+        $objPHPExcel->getActiveSheet()
+            ->getColumnDimension($col)
+            ->setAutoSize(true);
+    }
+    $objPHPExcel->setActiveSheetIndex(0);
+        }}
+
+
+    ob_end_clean();
+    ob_start();
+
+    // Configurações header para forçar o download
+    header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+    header ("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
+    header ("Cache-Control: no-cache, must-revalidate");
+    header ("Pragma: no-cache");
+    header ("Content-type: application/x-msexcel");
+    header ("Content-Disposition: attachment; filename=\"{$arquivo}\"" );
+    header ("Content-Description: PHP Generated Data" );
+    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+    $objWriter->save('php://output');
+    // Envia o conteúdo do arquivo
+    echo $html;
+    exit;
+}else{
+    echo "Nenhum item selecionado";
 }
 ?>
--- perfil/administrador/busca_result_relatorio.php
+++ perfil/administrador/busca_result_relatorio.php
@@ -7,6 +7,9 @@ $http = $server."/pdf/";
 
 $link02 = $http."excelEspecifica.php";
 
+?>
+<?php
+
 if (isset($_POST['per']) && ($_POST['per'] != '' AND $_POST['per2'])  && ($_POST['per2'] != '')){
 
     $pesquisar = $_POST['per'];
@@ -21,36 +24,35 @@ if (isset($_POST['per']) && ($_POST['per'] != '' AND $_POST['per2'])  && ($_POST
 $num = mysqli_num_rows($result);
 if($num > 0)
 {
-$i = 0;
-while($lista = mysqli_fetch_array($result))
-{
-$user = recuperaDados("users","id",$lista['users_id']);
-$category = recuperaDados("categories","id",$lista['categories_id']);
-$status = recuperaDados("problem_status","id",$lista['problem_status_id']);
-$x[$i]['id'] = $lista['id'];
-$x[$i]['local'] = $user['local'];
-$x[$i]['contact'] = $lista['contact'];
-$x[$i]['category'] = $category['category'];
-$x[$i]['description'] = substr($lista['description'],0,25);
-$x[$i]['startDate'] = exibirDataHoraBr($lista['startDate']);
-$x[$i]['status'] = $status['status'];
-$i++;
-}
-$x['num'] = $i;
-
-if($x['num'] == 1)
-{
-$mensagem = "Foi encontrado ".$x['num']." resultado.";
+    $i = 0;
+    while($lista = mysqli_fetch_array($result))
+    {
+        $user = recuperaDados("users","id",$lista['users_id']);
+        $category = recuperaDados("categories","id",$lista['categories_id']);
+        $status = recuperaDados("problem_status","id",$lista['problem_status_id']);
+        $x[$i]['id'] = $lista['id'];
+        $x[$i]['local'] = $user['local'];
+        $x[$i]['contact'] = $lista['contact'];
+        $x[$i]['category'] = $category['category'];
+        $x[$i]['description'] = substr($lista['description'],0,25);
+        $x[$i]['startDate'] = exibirDataHoraBr($lista['startDate']);
+        $x[$i]['status'] = $status['status'];
+        $i++;
+    }
+    $x['num'] = $i;
+    if($x['num'] == 1)
+    {
+        $mensagem = "Foi encontrado ".$x['num']." resultado.";
+    }
+    else
+    {
+        $mensagem = "Foram encontrados ".$x['num']." resultados.";
+    }
 }
 else
 {
-$mensagem = "Foram encontrados ".$x['num']." resultados.";
-}
-}
-else
-{
-$x['num'] = 0;
-$mensagem = "Nenhum resultado foi encontrado.";
+    $x['num'] = 0;
+    $mensagem = "Nenhum resultado foi encontrado.";
 }
 
 ?>
@@ -63,59 +65,94 @@ $mensagem = "Nenhum resultado foi encontrado.";
 			<h5><a href="?perfil=administrador&p=index">Fazer outra busca</a></h5>
             <form method="POST" action="<?php echo $link02; ?>">
                         <div class="btn-group mr-2">
-                            <button type="submit" class="btn btn-theme btn-block btn-sm position-relative" data-dismiss="modal">Gerar Planilha  </button>
+                            <button type="submit" class="btn btn-theme btn-md btn-block position-relative" style="border-radius: 30px;">&nbsp; Planilha Especifica  <img src="perfil\administrador\excel.png" style="height: 20px"/> &nbsp;&nbsp;</button>
                         </div>
+
+                <br><br>
+
+                <div class="row">
+                    <div class="col-md-offset-1 col-md-10">
+                        <div class="table-responsive list_info">
+                            <table class='table table-condensed'>
+                                <thead>
+                                <tr class='list_menu'>
+                                    <td><input type="checkbox" id="checkTodos" name="checkTodos"> <span>Selecionar Todos</span></td>
+                                    <td>Chamado nº</td>
+                                    <td>Local</td>
+                                    <td>Contato</td>
+                                    <td>Categoria</td>
+                                    <td>Descrição</td>
+                                    <td>Data Abertura</td>
+                                    <td>Status</td>
+                                    <td width="10%"></td>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                for($h = 0; $h < $x['num']; $h++)
+                                {
+                                ?>
+                                <tr>
+                                    <?php $id = $x[$h]['id']; ?>
+                                    <td class="text-center">
+                                        <?php echo "<input type='checkbox' name='relatorio[$id]' value='1'" ?>
+                                    </td>
+                                    <td class='list_description'><?php echo $x[$h]['id']; ?></td>
+                                    <td class='list_description'><?php echo $x[$h]['local']; ?></td>
+                                    <td class='list_description'><?php echo $x[$h]['contact']; ?></td>
+                                    <td class='list_description'><?php echo $x[$h]['category']; ?></td>
+                                    <td class='list_description'><?php echo $x[$h]['description']; ?></td>
+                                    <td class='list_description'><?php echo $x[$h]['status']; ?></td>
+                                    <td class='list_description'><?php echo $x[$h]['startDate']; ?></td>
+            </form>
+                                    <td class='list_description'>
+            <?php echo "
+            <form method='POST' action='?perfil=administrador&p=detalhes'>
+                <input type='hidden' name='idChamado' value='".$x[$h]['id']."' />
+                <input type ='submit' class='btn btn-theme btn-block' value='carregar'>
+            </form>
+            "; ?></td>
+                                </tr>
+
+                            <?php } ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-		</div>
-		<div class="row">
-			<div class="col-md-offset-1 col-md-10">
-				<div class="table-responsive list_info">
-					<table class='table table-condensed'>
-						<thead>
-							<tr class='list_menu'>
-                                <td></td>
-								<td>Chamado nº</td>
-								<td>Local</td>
-								<td>Contato</td>
-								<td>Categoria</td>
-								<td>Descrição</td>
-								<td>Data Abertura</td>
-								<td>Status</td>
-								<td width="10%"></td>
-							</tr>
-						</thead>
-						<tbody>
-							<?php
-							for($h = 0; $h < $x['num']; $h++)
-							{
-								echo "<tr>";
-                                $id = $x[$h]['id'];
-                                echo "<td class='list_description'>";
-                                echo "<input type='checkbox' name='relatorio[$id]' value='1'>";
-                                echo "</td>";
-								echo "<td class='list_description'>".$x[$h]['id']."</td>";
-								echo "<td class='list_description'>".$x[$h]['local']."</td>";
-								echo "<td class='list_description'>".$x[$h]['contact']."</td>";
-								echo "<td class='list_description'>".$x[$h]['category']."</td>";
-								echo "<td class='list_description'>".$x[$h]['description']."...</td>";
-								echo "<td class='list_description'>".$x[$h]['startDate']."</td>";
-								echo "<td class='list_description'>".$x[$h]['status']."</td>";
-								echo "<td class='list_description'>
-								</form>		
-								<form method='POST' action='?perfil=administrador&p=detalhes'>
-											<input type='hidden' name='idChamado' value='".$x[$h]['id']."' />
-											<input type ='submit' class='btn btn-theme btn-block' value='carregar'>
-										</form>
-									</td>";
-								echo "</tr>";
-							}
-							?>
-						</tbody>
-					</table>
-				</div>
-			</div>
+
+        </div>
+
+
+
+
+
+
+    </div>
+    </div>
+
 		</div>
 
 	</div>
 </section>
+
+
+ <!-- Script -->
+
+<script>
+    $("#checkTodos__").change(function () {
+        $("input:checkbox").prop('checked', $(this).prop("checked"));
+    });
+
+    $("#checkTodos__").click(function(){
+        $('input:checkbox').not(this).prop('checked', this.checked);
+    });
+
+    var checkTodos = $("#checkTodos");
+    checkTodos.click(function () {
+        if ( $(this).is(':checked') ){
+            $('input:checkbox').prop("checked", true);
+        }else{
+            $('input:checkbox').prop("checked", false);
+        }
+    });
+</script>
