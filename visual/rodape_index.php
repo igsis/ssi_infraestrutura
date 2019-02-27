cd0f05109275da9ac63d68c99c604506ff7419d5
Lorelei Gabriele <loreleigab@hotmail.com>
2019-02-27 13:51:08 -0300

    Removendo var_dump do rodapé

--- visual/rodape_index.php
+++ visual/rodape_index.php
@@ -21,11 +21,13 @@
 	<div class="container">
 		<div class="col-md-12">
 		<?php
+        /*
 			echo "<strong>SESSION</strong><pre>", var_dump($_SESSION), "</pre>";
 			echo "<strong>POST</strong><pre>", var_dump($_POST), "</pre>";
 			echo "<strong>GET</strong><pre>", var_dump($_GET), "</pre>";
 			echo "<strong>FILES</strong><pre>", var_dump($_FILES), "</pre>";
 			echo ini_get('session.gc_maxlifetime')/60; // em minutos
+        */
 		?>
 		</div>
 	</div>
